7cf418b5f9fa6950bbedf194b730de2203867f22
Guilherme-Silva-Oliveira <guilherme.soliveira@sptech.school>
2025-05-01 17:34:09 -0300

    API de Cadastro e Login Implementada

---
+++ ScriptTabelas.sql
@@ -0,0 +1,17 @@
+create database ProjetoIndividual;
+use ProjetoIndividual;
+
+create table Usuarios (
+idUser int primary key auto_increment,
+nome varchar(50),
+email varchar(50),
+senha varchar(20),
+raca varchar(15),
+favorito varchar(20)
+);
+/*
+create table Formulario (
+idFormulario int primary key auto_increment,
+
+);
+*/
