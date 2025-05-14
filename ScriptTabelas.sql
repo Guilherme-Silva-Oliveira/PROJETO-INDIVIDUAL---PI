cd76ebf39f735ce2b21447b27a2d660c4f7bc07b
Guilherme-Silva-Oliveira <guilherme.soliveira@sptech.school>
2025-05-13 22:26:55 -0300

    Modelagem e Script de Tabelas Atualizado

--- ScriptTabelas.sql
+++ ScriptTabelas.sql
@@ -4,23 +4,40 @@ use ProjetoIndividual;
 create table Usuarios (
 idUser int primary key auto_increment,
 nome varchar(50),
-email varchar(50),
+email varchar(50) unique,
 senha varchar(20),
 raca varchar(15),
 favorito varchar(20)
 );
 
-create table ResultadoQuiz (
-idResposta int primary key auto_increment,
-pergunta1 varchar(20),
-pergunta2 varchar(20),
-pergunta3 varchar(20),
-pergunta4 varchar(20),
-pergunta5 varchar(20),
-pergunta6 varchar(20),
-pergunta7 varchar(20),
-pergunta8 varchar(20)
+create table PerguntaQuiz (
+idPergunta int primary key auto_increment,
+enunciado varchar(50),
+alt_A varchar(40),
+alt_B varchar(40),
+alt_C varchar(40),
+alt_D varchar(40)
 );
 
+create table RespostaUsuario (
+idRespostaUsuario int primary key auto_increment,
+resposta varchar(40),
+fk_Usuario int,
+fk_Pergunta int,
+foreign key fk_Usuario(fk_Usuario) references Usuarios(idUser),
+foreign key fk_Pergunta(fk_Pergunta) references PerguntaQuiz(idPergunta)
+);
+
+insert into PerguntaQuiz (enunciado, alt_A, alt_B, alt_C, alt_D) values
+('Qual o seu Contato com o Jogo?','Já Joguei','Apenas Assisti','Já Ouvi Falar','Nunca Joguei'),
+('Se Já Jogou, Por Quantas Horas?','Menos de 15 Horas','Entre 15 e 40 Horas','Mais de 40 Horas','Nunca Joguei'),
+('Qual seu Estilo de Jogo Favorito?','Ação','Aventura','História e Narrativa','Não Gosto de Jogar'),
+('Diga qual a Opção mais lhe Descreve','Força Bruta','Magias Vanir','Lâminas de Fogo','Machado de Gelo'),
+('Qual seu Lema?','Guerra é a Primeira Opção','Manter a Paz a Todo Custo','Confiança é uma Conquista','Salvar Vidas em Primeiro Lugar'),
+('Qual Reino Gostaria de Visitar?','Svartalfheim Reino dos Anões','Muspelheim Reino do Fogo','Vanaheim Reino dos Deuses Vanir','Asgard Reino dos Deuses Vanir'),
+('Recomendaria este Jogo para Alguém?','Sim','Não',null,null);
+
 select * from Usuarios;
-select * from Respostas;
+select * from RespostaUsuario;
+delete from Usuarios;
+
