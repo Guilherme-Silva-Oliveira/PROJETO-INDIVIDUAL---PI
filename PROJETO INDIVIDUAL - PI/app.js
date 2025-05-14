ecb0f13de92af0b805f0acc17005af6fbbc97ba6
Guilherme-Silva-Oliveira <guilherme.soliveira@sptech.school>
2025-05-13 22:15:28 -0300

    Corrigindo Commit do Quizz

--- PROJETO INDIVIDUAL - PI/app.js
+++ PROJETO INDIVIDUAL - PI/app.js
@@ -17,7 +17,6 @@ var app = express();
 
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
-var perguntaRouter = require("./src/routes/perguntas");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -27,7 +26,6 @@ app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
-app.use("/perguntas", perguntaRouter);
 
 app.listen(PORTA_APP, function () {
     console.log(`
